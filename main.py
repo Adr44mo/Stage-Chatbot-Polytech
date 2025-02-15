c7b25aea81f7640884afe8b0c9d8cbac0353395f
Yannick <yannickzhang23@gmail.com>
2025-02-15 17:11:23 +0100

    integration chroma pour gpt-4o

--- main.py
+++ main.py
@@ -14,7 +14,12 @@ import requests
 # Imports d'elements specifiques externes
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.chat_models import ChatOpenAI
-from langchain_community.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS, Chroma
+from langchain.schema import Document
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel, Field
+from typing import List, Optional
 from openai import OpenAI
 
 # Affichage d'un message de succès d'import des librairies
@@ -129,6 +134,79 @@ def save_history(user_id, history):
     except Exception as e:
         print("Erreur lors de la sauvegarde de l'historique :", e)
 
+# ----------------------------------------------------------------------------------------------
+# Recherche des documents dans Chroma en appliquant des filtres sur la spécialité et le statut
+# ----------------------------------------------------------------------------------------------
+
+# Chargement de la base de données Chroma avec les embeddings d'OpenAI
+vectordb = Chroma(
+    persist_directory="./Json1",    # Répertoire pour stocker les vecteurs et les métadonnées
+    embedding_function=OpenAIEmbeddings(model="text-embedding-3-small"), 
+    collection_name="Json1"  
+)
+
+# Définition du modèle de recherche avec Pydantic pour valider la structure des requêtes
+class Search(BaseModel):
+    query: str = Field(..., description="Recherche par similarité appliquée à des documents d'informations sur l'école")
+    specialty: Optional[str] = Field(None, description="Spécialité à rechercher (exemple: MAIN, INFO, MATH)")
+    status: Optional[str] = Field(None, description="Statut du document, peut être 'publique' ou 'privé'")
+
+# Fonction pour récupérer un document par son ID depuis la base de données Chroma
+def get_document_by_id(collection, vector_id):
+    results = collection.get(where={"ID": vector_id})
+    if results and results["ids"]:
+        return results 
+    else:
+        return None
+
+# Fonction de recherche dans Chroma en fonction d'un critère spécifique (spécialité, statut)
+def retrieval_single_field(search: Search, field: str, value) -> List[str]:
+    _filter = {}
+    if field == "Specialty":
+        _filter["Specialty"] = search.specialty
+    if field == "Status":
+        _filter["Status"] = search.status
+    
+    results = vectordb.similarity_search(search.query, filter=_filter)
+    return [doc.metadata.get("ID", 'None') for doc in results]
+
+# Recherche des documents dans Chroma en appliquant des filtres sur la spécialité et le statut.
+def retrieval(search: Search) -> List[Document]:
+    all_ids_set = set() 
+
+    if search.specialty:
+        all_ids_set.update(retrieval_single_field(search, "Specialty", search.specialty))
+    if search.status:
+        all_ids_set.update(retrieval_single_field(search, "Status", search.status))
+
+    return list(all_ids_set)
+
+# Fonction de détection des filtres dans la question de l'utilisateur (extraction des spécialités)
+def filter_detection(question) -> dict:
+    SPECIALTIES = [
+        "AGRAL (Agroalimentaire)", 
+        "EISE (Électronique - Informatique Parcours systèmes embarqués)",
+        "EI2I (Électronique - Informatique Parcours informatique industrielle)", 
+        "GM (Génie Mécanique)",
+        "MAIN (Mathématiques appliquées et informatique)", 
+        "MTX (Matériaux - Chimie)", 
+        "ROB (Robotique)",
+        "ST (Sciences de la terre : aménagement, environnement, énergie)"
+    ]
+    
+    llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0) 
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "Tu es un assistant chargé d'extraire des filtres de métadonnées à partir d'une question. Voici les filtres qui nous intéressent : specialty. Plusieurs informations peuvent être ajoutées dans le même filtre. Voici une liste des spécialités reconnues : {SPECIALTIES}."),
+        ("human", "Voici la question de l'utilisateur : {question}. Retourne les informations au format JSON avec 'None' si rien ne correspond.")
+    ])
+
+    # Chaîne de traitement pour passer la question au modèle
+    FilterChain = (prompt | llm | RunnablePassthrough())
+    response = FilterChain.invoke({"question": question, "SPECIALTIES":SPECIALTIES})
+    response = str(response.content)
+    dict_answer = json.loads(response)
+    return dict_answer
+
 # ---------------------------------------------------
 # Initialisation du LLM et du Retriever
 # ---------------------------------------------------
@@ -150,9 +228,13 @@ def init_llm_and_retriever(model_choice):
     else:
         raise ValueError("Type d'embeddings inconnu ou non géré.")
     
-    # Chargement de l'index FAISS
-    vector = FAISS.load_local(faiss_index_path, embeddings, allow_dangerous_deserialization=True)
-    retriever = vector.as_retriever()
+    # Chargement de l'index FAISS / Chroma
+    if model_choice == "gpt-4o":
+        vector = vectordb
+        retriever = vector.as_retriever()
+    else:
+        vector = FAISS.load_local(faiss_index_path, embeddings, allow_dangerous_deserialization=True)
+        retriever = vector.as_retriever()
 
     # GPT : on definit la variable llm
     if model_choice in ["gpt-4o", "gpt-4o-mini"]:
@@ -268,13 +350,6 @@ def show_ui():
             with st.chat_message("user"):
                 st.markdown(user_input)
             try:
-                # # Si l'utilisateur a choisi llama, on n'utilise pas de retriever
-                # # Cela doit etre mis a jour dans la prochaine version sans bug
-                # if model_choice == "llama3" :
-                #     response = query_norag(llm, retriever, user_input, st.session_state["chat_history"])
-                # # Si l'utilisateur a choisi gpt, on utilise bel et bien un retriever
-                # # Ce retriever est deja pleinement fonctionnel
-                # else : 
                 response = query_rag(llm, retriever, user_input, st.session_state["chat_history"])
             except requests.exceptions.ConnectionError:
                 st.error(
@@ -292,6 +367,24 @@ def show_ui():
                 bot_answer = response.content
             else:
                 bot_answer = str(response)
+
+            # Vérification des liens pour cette question
+            if model_choice == "gpt-4o":
+                dict_answer = filter_detection(user_input)
+                spec = dict_answer.get('specialty', [])
+                search_input = Search(query=user_input, specialty=", ".join(spec), status="publique")
+                retrieved_documents = retrieval(search_input)
+
+                document_urls = set()  # Utilisation d'un set pour éviter les doublons
+                for doc_id in retrieved_documents:
+                    doc = get_document_by_id(vectordb, doc_id)
+                    url = doc['metadatas'][0].get('URL', None)
+                    if url and url:
+                        document_urls.add(url)
+
+                if document_urls:
+                    urls_str = ", ".join([f"{url}" for url in document_urls])
+                    bot_answer += "\n\nSource : " + urls_str
 
             # On met a jour l'historique pour y ajouter la reponse
             with st.chat_message("assistant", avatar="web/assets/logopolytech.svg"):
