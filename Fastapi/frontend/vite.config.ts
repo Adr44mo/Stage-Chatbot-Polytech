cb3954335b5fc58001728ddbe6bc2119b3c43705
Adr44mo <adrien.morille44@gmail.com>
2025-06-19 15:34:28 +0200

    vite config

--- Fastapi/frontend/vite.config.ts
+++ Fastapi/frontend/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/files': 'http://134.157.105.72:8000'
+    }
+  }
+});
